736b45e01dde213656296f8e2323f980791b952d
invassiion <invassiion@gmail.com>
2024-04-20 16:19:37 +0700

    Создание проекта, создание конфигурации для базовой авторизации пользователей, создание таблицы и репозитория в бд

---
+++ src/main/java/com/example/medClinic/MedClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.medClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medClinic/MedClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
